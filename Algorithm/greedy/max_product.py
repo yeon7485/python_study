8fe46e0e66ceb223878e27759928c1945d1e8192
yeon7485 <wsx4379@naver.com>
2021-06-13 00:05:24 +0900

    greedy algorithm

---
+++ Algorithm/greedy/max_product.py
@@ -0,0 +1,26 @@
+# 최대 곱 구하기
+
+def max_product(card_lists):
+    # 누적된 곱을 저장하는 변수
+    product = 1
+
+    # 반복문을 돌면서 카드 뭉치를 하나씩 본다
+    for card_list in card_lists:
+        # product에 각 뭉치의 최댓값을 곱해 준다
+        product *= max(card_list)
+
+    return product
+
+
+# 테스트
+test_cards1 = [[1, 6, 5], [4, 2, 3]]
+print(max_product(test_cards1))
+
+test_cards2 = [[9, 7, 8], [9, 2, 3], [9, 8, 1], [2, 8, 3], [1, 3, 6], [7, 7, 4]]
+print(max_product(test_cards2))
+
+test_cards3 = [[1, 2, 3], [4, 6, 1], [8, 2, 4], [3, 2, 5], [5, 2, 3], [3, 2, 1]]
+print(max_product(test_cards3))
+
+test_cards4 = [[5, 5, 5], [4, 3, 5], [1, 1, 1], [9, 8, 3], [2, 8, 4], [5, 7, 4]]
+print(max_product(test_cards4))
