716affdf055d986e5db9c1f92d27b6e10156a49c
yeon7485 <wsx4379@naver.com>
2021-06-15 03:34:48 +0900

    algorithm quiz2

---
+++ Algorithm/quiz/find_same_number_1.py
@@ -0,0 +1,17 @@
+def find_same_number(some_list):
+    # 이미 나온 요소를 저장시켜줄 사전
+    number = {}
+
+    for element in some_list:
+        # 이미 나온 요소인지 확인하고 맞으면 요소를 리턴한다
+        if element in number:
+            return element
+
+        # 해당 요소를 사전에 저장시킨다.
+        number[element] = True
+
+
+# 테스트
+print(find_same_number([1, 4, 3, 5, 3, 2]))
+print(find_same_number([4, 1, 5, 2, 3, 5]))
+print(find_same_number([5, 2, 3, 4, 1, 6, 7, 8, 9, 3]))
